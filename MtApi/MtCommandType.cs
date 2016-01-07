eceb71fd40a3a562e46e4be3efa157c0d0340cc6
vdemydiuk <vdemydiuk@gmail.com>
2016-01-07 11:40:05 +0200

    Started to work on TerminalInfoString

--- MtApi/MtCommandType.cs
+++ MtApi/MtCommandType.cs
@@ -185,6 +185,9 @@ namespace MtApi
         iTimeArray                          = 149,
 
         //
-        RefreshRates                        = 150
+        RefreshRates                        = 150,
+        //
+        TerminalInfoString                  = 153,
+        SymbolInfoString                    = 154
     }
 }
---
+++ MtApi/MtTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MtApi
+{
+    class MtTypes
+    {
+    }
+
+    public enum ENUM_TERMINAL_INFO_STRING
+    {
+
+    }
+}
