486c42685a2018fc4cfc4976d99b568152d3e25a
DW <vdemydiuk@gmail.com>
2017-09-26 18:37:50 +0300

    Issue #41: Added Object Types enum

--- MtApi5/Mt5Enums.cs
+++ MtApi5/Mt5Enums.cs
@@ -606,4 +606,55 @@
 
     #endregion //Trade Orders in Depth Of Market
 
+    #region Object Types
+
+    public enum ENUM_OBJECT
+    {
+        OBJ_VLINE = 0,              // Vertical Line
+        OBJ_HLINE = 1,              // Horizontal Line
+        OBJ_TREND = 2,              // Trend Line
+        OBJ_TRENDBYANGLE = 3,       // Trend Line By Angle
+        OBJ_CYCLES = 4,             // Cycle Lines
+        OBJ_ARROWED_LINE = 108,     // Arrowed Line
+        OBJ_CHANNEL = 5,            // Equidistant Channel
+        OBJ_STDDEVCHANNEL = 6,      // Standard Deviation Channel
+        OBJ_REGRESSION = 7,         // Linear Regression Channel
+        OBJ_PITCHFORK = 8,          // Andrews Pitchfork
+        OBJ_GANNLINE = 9,           // Gann Line
+        OBJ_GANNFAN = 10,           // Gann Fan
+        OBJ_GANNGRID = 11,          // Gann Grid
+        OBJ_FIBO = 12,              // Fibonacci Retracement
+        OBJ_FIBOTIMES = 13,         // Fibonacci Time Zones
+        OBJ_FIBOFAN = 14,           // Fibonacci Fan
+        OBJ_FIBOARC = 15,           // Fibonacci Arcs
+        OBJ_FIBOCHANNEL = 16,       // Fibonacci Channel
+        OBJ_EXPANSION = 17,         // Fibonacci Expansion
+        OBJ_ELLIOTWAVE5 = 18,       // Elliott Motive Wave
+        OBJ_ELLIOTWAVE3 = 19,       // Elliott Correction Wave
+        OBJ_RECTANGLE = 20,         // Rectangle
+        OBJ_TRIANGLE = 21,          // Triangle
+        OBJ_ELLIPSE = 22,           // Ellipse
+        OBJ_ARROW_THUMB_UP = 23,    // Thumbs Up
+        OBJ_ARROW_THUMB_DOWN = 24,  // Thumbs Down
+        OBJ_ARROW_UP = 25,          // Arrow Up
+        OBJ_ARROW_DOWN = 26,        // Arrow Down
+        OBJ_ARROW_STOP = 27,        // Stop Sign
+        OBJ_ARROW_CHECK = 28,       // Check Sign
+        OBJ_ARROW_LEFT_PRICE = 29,  // Left Price Label
+        OBJ_ARROW_RIGHT_PRICE = 30, // Right Price Label
+        OBJ_ARROW_BUY = 31,         // Buy Sign
+        OBJ_ARROW_SELL = 32,        // Sell Sign
+        OBJ_ARROW = 100,            // Arrow
+        OBJ_TEXT = 101,             // Text
+        OBJ_LABEL = 102,            // Label
+        OBJ_BUTTON = 103,           // Button
+        OBJ_CHART = 104,            // Chart
+        OBJ_BITMAP = 105,           // Bitmap
+        OBJ_BITMAP_LABEL = 106,     // Bitmap Label
+        OBJ_EDIT = 107,             // Edit
+        OBJ_EVENT = 109,            // The "Event" object corresponding to an event in the economic calendar
+        OBJ_RECTANGLE_LABEL = 110   // The "Rectangle label" object for creating and designing the custom graphical interface.
+    }
+
+    #endregion // Object Types
 }
