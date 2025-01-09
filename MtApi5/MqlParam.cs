6b3d54b9d50f6883e26b821474ce93b013d30fb9
Viacheslav Demydiuk <vdemydiuk@gmail.com>
2025-01-09 17:06:36 +0200

    MtApi5: added default value to properites of MqlParam for correct json serialization

--- MtApi5/MqlParam.cs
+++ MtApi5/MqlParam.cs
@@ -3,8 +3,8 @@
     public class MqlParam
     {
         public ENUM_DATATYPE DataType { get; set; }
-        public long? IntegerValue { get; set; }
-        public double? DoubleValue { get; set; }
-        public string? StringValue { get; set; }
+        public long IntegerValue { get; set; } = 0;
+        public double DoubleValue { get; set; } = 0.0;
+        public string StringValue { get; set; } = String.Empty;
     }
 }
