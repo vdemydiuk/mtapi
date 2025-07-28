ff871e5e06d67efcd538107a83d5f22a3865055b
Viacheslav Demydiuk <vdemydiuk@gmail.com>
2025-07-28 22:57:46 +0300

    MtApi5: implement function iFractals instead of mistake function iForce

--- MtApi5/MtApi5Client.cs
+++ MtApi5/MtApi5Client.cs
@@ -2710,14 +2710,14 @@ namespace MtApi5
         }
 
         ///<summary>
-        ///The function returns the handle of the Force Index indicator.
+        ///The function returns the handle of the Fractals indicator.
         ///</summary>
         ///<param name="symbol">The symbol name of the security, the data of which should be used to calculate the indicator.</param>
         ///<param name="period">The value of the period can be one of the ENUM_TIMEFRAMES enumeration values, 0 means the current timeframe.</param>
-        public int iForce(string symbol, ENUM_TIMEFRAMES period)
+        public int iFractals(string symbol, ENUM_TIMEFRAMES period)
         {
             Dictionary<string, object> cmdParams = new() { { "Symbol", symbol ?? string.Empty }, { "Period", (int)period } };
-            return SendCommand<int>(ExecutorHandle, Mt5CommandType.iForce, cmdParams);
+            return SendCommand<int>(ExecutorHandle, Mt5CommandType.iFractals, cmdParams);
         }
 
         ///<summary>
